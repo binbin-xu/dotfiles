8ee0851e6dffab1c1e45d559111e71d0dd8b61df
Binbin Xu <b.xu17@imperial.ac.uk>
2020-05-28 17:40:41 +0100

    Add option aliases for git reset

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -73,7 +73,10 @@ alias gdc='git diff --cached'
 alias gD='git ls-files'
 alias gag='git exec ag'
 #git reset
+alias grh='git reset'  # (HEAD)
 alias grhh='git reset --hard'
+alias grso='git reset --soft origin/$(git_current_branch)'
+alias grho='git reset --hard origin/$(git_current_branch)'
 #git log
 alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias glogs="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --stat --all"
