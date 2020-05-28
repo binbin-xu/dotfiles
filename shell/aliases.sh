66da861935a2b6335d65465b6b21b6f81f30d19f
Binbin Xu <b.xu17@imperial.ac.uk>
2020-05-28 17:34:05 +0100

    Add --all info to glogs

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -75,7 +75,7 @@ alias gag='git exec ag'
 alias grhh='git reset --hard'
 #git log
 alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-alias glogs="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --stat"
+alias glogs="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --stat --all"
 
 # tensorboard 
 alias tb='tensorboard --logdir '
