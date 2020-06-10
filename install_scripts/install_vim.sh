91c9ce2e1f10066c884093cb5e6eb16d57032d4b
Binbin Xu <b.xu17@imperial.ac.uk>
2020-06-10 11:13:11 +0100

    Update vim install_scripts

--- install_scripts/install_vim.sh
+++ install_scripts/install_vim.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+echo_error () {
+    echo -e "\033[31mError: $*\033[0m"
+    exit 1
+}
 
-if [ "$(uname)" == "Linux" ]; then
+case "$(uname -s)" in
+    # 'Mac OS X'
+    Darwin)
+        brew upgrade vim
+        ;;
+
+    Linux)
         sudo add-apt-repository ppa:jonathonf/vim
         sudo apt update
-        sudo apt install vim
-fi
+        sudo apt install vim-gnome
+        ;;
+
+    CYGWIN*|MINGW32*|MSYS*)
+        echo_error "unsupported platform"
+        ;;
+
+    *)
+        echo_error "unsupported platform"
+        ;;
+esac
