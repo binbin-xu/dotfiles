60b058c62cb693c94c154dec09ba1202c4a26191
Binbin Xu <b.xu17@imperial.ac.uk>
2020-06-18 11:25:17 +0100

    Update conda project install

--- scripts/conda_project/install/install.sh
+++ scripts/conda_project/install/install.sh
@@ -1,31 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
-set -e
-
-echo_bold () {
-  echo -e "\033[1m$*\033[0m"
-}
-
-echo_warning () {
-  echo -e "\033[33m$*\033[0m"
-}
-
-conda_check_installed () {
-  if [ ! $# -eq 1 ]; then
-    echo "usage: $0 PACKAGE_NAME"
-    return 1
-  fi
-  conda list | awk '{print $1}' | egrep "^$1$" &>/dev/null
-}
-
-HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT=$HERE/..
+HERE=$(realpath $(dirname ${BASH_SOURCE[0]}))
+source $HERE/__init__.sh
+ROOT=$(realpath $HERE/..)
 
 cd $ROOT
 
 source .anaconda3/bin/activate
 
-# ---------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
 
 echo_bold "==> Installing the right pip and dependencies for the fresh python"
 pip install --upgrade pip 
