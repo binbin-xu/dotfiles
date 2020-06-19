621de43fef3d7262dc25340ac3839b6bf82e6a71
Binbin Xu <b.xu17@imperial.ac.uk>
2020-06-19 16:12:51 +0100

    Add cuda 10.0 install_scripts

---
+++ install_scripts/install_cuda_toolkit_10.0.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# this corrsponds to driver 440 + cuda 10.2
+cd $(mktemp -d)
+
+if [ "$(lsb_release -sc)" = "bionic" ]; then
+  wget https://developer.download.nvidia.com/compute/cuda/10.0/secure/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+  sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
+  sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
+  sudo apt-get update
+  sudo apt-get install cuda-toolkit-10-0
+elif [ "$(lsb_release -sc)" = "xenial" ]; then
+  wget https://developer.download.nvidia.com/compute/cuda/10.0/secure/Prod/local_installers/cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+  sudo dpkg -i cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64.deb                                 
+  sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
+  sudo apt-get update
+  sudo apt-get install cuda-toolkit-10-0
+else
+  echo "[$0] Unsupported OS: $(uname), $(lsb_release -sc)"
+  exit 1
+fi
+
+cd -
