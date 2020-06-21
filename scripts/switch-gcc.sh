8656079452b616e76e92ac5aad69ad491389496c
Binbin Xu <b.xu17@imperial.ac.uk>
2020-06-21 22:34:38 +0100

    Update gcc verison switch script

--- scripts/switch-gcc.sh
+++ scripts/switch-gcc.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# current version
+# check existing gcc versions
 ll /usr/bin/gcc*
 
 # install gcc 4.8 and 5
-sudo apt install gcc-4.8 gcc-4.8-multilib g++-4.8 g++-4.8-multilib
+sudo apt install gcc-4.9 gcc-4.9-multilib g++-4.9 g++-4.9-multilib
 sudo apt install gcc-5 gcc-5--multilib g++-5 g++-5--multilib
 
 # set update-alternatives
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 50 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 
